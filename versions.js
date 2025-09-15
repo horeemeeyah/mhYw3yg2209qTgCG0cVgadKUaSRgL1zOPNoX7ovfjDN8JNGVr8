4a0d075ce9e159a6569e688039e9d1a99df4d959
horeemeeyah <liliisdagoat62@gmail.com>
2025-09-15 18:09:27 -0500

    Update versions.js

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 // This file is auto-generated
-const VALID_VERSION_IDS = [""];
+const VALID_VERSION_IDS = ["v2_initial"];
 function checkVersionValidity(version) {
     return VALID_VERSION_IDS.includes(version);
 }
+
 
